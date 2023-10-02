3c64f3ce5e713cb315226c4ce9e5000e7a3a3663
alexis.desgrange <alexis.desgrange@diiage.org>
2023-10-02 16:11:49 +0200

    Init Nuxt 3 with Vuetify 3

---
+++ front-jukebox/nuxt.config.ts
@@ -0,0 +1,22 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    //...
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+})
