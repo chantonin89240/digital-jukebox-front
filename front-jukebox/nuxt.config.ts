2c94826a741b332d93b500f382bb16eca1fb8fca
alexis.desgrange <alexis.desgrange@diiage.org>
2023-10-02 16:34:05 +0200

    Ajout esLint et prettier

--- front-jukebox/nuxt.config.ts
+++ front-jukebox/nuxt.config.ts
@@ -1,14 +1,14 @@
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
         // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }))
-      })
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
     },
     //...
   ],
@@ -19,4 +19,4 @@ export default defineNuxtConfig({
       },
     },
   },
-})
+});
--- front-jukebox/plugins/vuetify.ts
+++ front-jukebox/plugins/vuetify.ts
@@ -1,10 +1,8 @@
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
 
 export default defineNuxtPlugin((app) => {
-  const vuetify = createVuetify({
-    
-  })
-  app.vueApp.use(vuetify)
-})
+  const vuetify = createVuetify({});
+  app.vueApp.use(vuetify);
+});
