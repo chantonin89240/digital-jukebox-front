3cdd6084474b69d4ab792d171fde2bf6ffb65679
Lucas Alves <Lucas.Alves@diiage.org>
2023-12-04 13:14:28 +0000

    Revert "Merged PR 31: Ajout de l'internationalization (i18n)
    Ajout de l'internationalization (i18n)"

--- front-jukebox/app.vue
+++ front-jukebox/app.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <NuxtLayout>
-      <!--p>Exemple d'utilisation de l'API d'internationalization: {{ $t("welcome") }}</p-->
       <NuxtPage/>
     </NuxtLayout>
   </div>
--- front-jukebox/nuxt.config.ts
+++ front-jukebox/nuxt.config.ts
@@ -8,31 +8,14 @@ export default defineNuxtConfig({
   css: ['@mdi/font/css/materialdesignicons.min.css'],
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/i18n',
     (_options, nuxt) => {
       nuxt.hooks.hook("vite:extendConfig", (config) => {
-        // ts-expect-error
-        //config.plugins.push(vuetify({ autoImport: true }));
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
       });
     },
     //...
   ],
-  i18n: {
-    vueI18n: './i18n.config.ts',
-    locales: [ 
-      { 
-        code:'en',
-        file:'en.json' 
-      },
-      { 
-        code:'fr',
-        file:'fr.json' 
-      }
-    ],
-    langDir:'locales',
-    defaultLocale: 'en',
-    lazy: true
-  },
   vite: {
     vue: {
       template: {
@@ -41,14 +24,3 @@ export default defineNuxtConfig({
     },
   },
 });
-
-/*
-({
-  modules: [
-    '@nuxtjs/i18n'
-  ],
-  i18n: {
-    vueI18n: './i18n.config.ts' // if you are using custom path, default 
-  }
-})
-  */
