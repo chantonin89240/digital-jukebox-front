bb7614188d0945b66d5cba915eb51f7b204b2ef9
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2024-01-08 11:17:42 +0100

    installation de pinia

--- front-jukebox/nuxt.config.ts
+++ front-jukebox/nuxt.config.ts
@@ -22,6 +22,7 @@ export default defineNuxtConfig({
   css: ['@mdi/font/css/materialdesignicons.min.css'],
   devtools: { enabled: true },
   modules: [
+    '@pinia/nuxt',
     [ 
       '@nuxtjs/i18n',
       { /* i18n options */
